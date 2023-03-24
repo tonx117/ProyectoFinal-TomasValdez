c20b686636f932c768d0bb715aaf0e6929e04470
tonx117 <tomasvaldez02@gmail.com>
2023-03-24 16:19:19 -0300

    a
    a

--- Proyecto-Final-TV/src/app/app.module.ts
+++ Proyecto-Final-TV/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
